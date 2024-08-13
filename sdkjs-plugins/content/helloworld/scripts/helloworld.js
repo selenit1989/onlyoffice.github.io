9bd8261a80ac3537ff5d4e8a376d94c90d930caa
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2024-08-13 17:02:23 +0500

    helloworld.js mod

--- sdkjs-plugins/content/helloworld/scripts/helloworld.js
+++ sdkjs-plugins/content/helloworld/scripts/helloworld.js
@@ -5,47 +5,53 @@
   window.Asc.plugin.init = function(){ 
       this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
   
-      var variant = 2;
+      // var variant = 2;
 
-      switch (variant)
-      {
-          case 0:
-          {
-              var sScript = "var oDocument = Api.GetDocument();";
-              sScript += "oParagraph = Api.CreateParagraph();";
-              sScript += "oParagraph.AddText('Hello world!');";
-              sScript += "oDocument.InsertContent([oParagraph]);";
-              this.info.recalculate = true;
-              this.executeCommand("close", sScript);
-              break;
-          }
-          case 1:
-          {
-              this.callCommand(function() {
-                  var oDocument = Api.GetDocument();
-                  var oParagraph = Api.CreateParagraph();
-                  oParagraph.AddText("Hello world!");
-                  oDocument.InsertContent([oParagraph]);
-              }, true);
-              break;
-          }
-          case 2:
-          {
-              Asc.scope.text = text; 
-              this.callCommand(function() {
-                  var oDocument = Api.GetDocument();
-                  var oParagraph = Api.CreateParagraph();
-                  oParagraph.AddText(Asc.scope.text);
-                  oDocument.InsertContent([oParagraph]);
-              }, true);
-              break;
-          }
-          default:
-              break;
-      }
+      // switch (variant)
+      // {
+      //     case 0:
+      //     {
+      //         var sScript = "var oDocument = Api.GetDocument();";
+      //         sScript += "oParagraph = Api.CreateParagraph();";
+      //         sScript += "oParagraph.AddText('Hello world!');";
+      //         sScript += "oDocument.InsertContent([oParagraph]);";
+      //         this.info.recalculate = true;
+      //         this.executeCommand("close", sScript);
+      //         break;
+      //     }
+      //     case 1:
+      //     {
+      //         this.callCommand(function() {
+      //             var oDocument = Api.GetDocument();
+      //             var oParagraph = Api.CreateParagraph();
+      //             oParagraph.AddText("Hello world!");
+      //             oDocument.InsertContent([oParagraph]);
+      //         }, true);
+      //         break;
+      //     }
+      //     case 2:
+      //     {
+      //         Asc.scope.text = text; 
+      //         this.callCommand(function() {
+      //             var oDocument = Api.GetDocument();
+      //             var oParagraph = Api.CreateParagraph();
+      //             oParagraph.AddText(Asc.scope.text);
+      //             oDocument.InsertContent([oParagraph]);
+      //         }, true);
+      //         break;
+      //     }
+      //     default:
+      //         break;
+      // }
 
       this.attachToolbarMenuClickEvent("insertPhrase", function(data) {
-          console.log("TRIGGERED");
+        Asc.scope.text = text; 
+        this.callCommand(function() {
+          var oDocument = Api.GetDocument();
+          var oParagraph = Api.CreateParagraph();
+          oParagraph.AddText(Asc.scope.text);
+          oDocument.InsertContent([oParagraph]);
+      }, true);
       });
   };
   
