b9b67d4c80890c00ea77a2df45ebb631d6561706
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2024-08-15 19:07:47 +0500

    config + helloworld.js mod

--- sdkjs-plugins/content/helloworld/scripts/helloworld.js
+++ sdkjs-plugins/content/helloworld/scripts/helloworld.js
@@ -1,41 +1,43 @@
 (function(window, undefined){
-
-  window.Asc.plugin.init = function(){  
-      this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
-
-      this.attachToolbarMenuClickEvent("insertPhrase", function(data) {
-        console.log("the data is" + data)
-        this.callCommand(function() {
-          var oDocument = Api.GetDocument();
-          var oParagraph = Api.CreateParagraph();
-          oParagraph.AddText("hello world");
-          oDocument.InsertContent([oParagraph]);
-      }, true);
-      });
-   
-  function getToolbarItems() {
-      console.log ()
-      let items = {
-          guid: window.Asc.plugin.info.guid,
-          tabs: [{
-              id: "hello_world_tab",
-              text: "Hello World",
-              items: [
-              {
-                  id: "insertPhrase",
-                  type: "button",
-                  text: "Insert",
-                  hint: "insert Hello World phrase",
-                  icons: "resources/buttons/icon.png", 
-                  lockInViewMode: true,
-                  enableToggle: false,
-                  separator: false
-              }
-              ]
-          }]
-      };
-      
-      return items;
-  }    
-};
-})(window);
+    
+    window.Asc.plugin.init = function(){  
+        this.executeMethod("AddToolbarMenuItem", [getToolbarItems()]);
+  
+        this.attachToolbarMenuClickEvent("insertPhrase", function(data) {
+          this.callCommand(function() {
+            var oDocument = Api.GetDocument();
+            var oParagraph = Api.CreateParagraph();
+            oParagraph.AddText("Hello world");
+            oDocument.InsertContent([oParagraph]);
+        }, true);
+        });
+     
+    function getToolbarItems() {
+        console.log ()
+        let items = {
+            guid: window.Asc.plugin.info.guid,
+            tabs: [{
+                id: "hello_world_tab",
+                text: "Hello World",
+                items: [
+                {
+                    id: "insertPhrase",
+                    type: "button",
+                    text: "Insert",
+                    hint: "insert Hello World phrase",
+                    icons: "resources/buttons/icon.png", 
+                    lockInViewMode: true,
+                    enableToggle: false,
+                    separator: false
+                }
+                ]
+            }]
+        };
+        
+        return items;
+    }    
+  };
+    
+  
+  })(window);
+  
